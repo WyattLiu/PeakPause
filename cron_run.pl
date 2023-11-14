22557587a5f8c130e4798929f0b7aad183049247
Wyatt Liu <wyatt_liu@hotmail.com>
2023-11-14 00:22:17 -0500

    latest cron

--- cron_run.pl
+++ cron_run.pl
@@ -7,15 +7,67 @@ use File::Basename;
 my $dirname = dirname(__FILE__);
 print "Dir name: $dirname\n";
 
-my $off_peak_start = 19;
-my $off_peak_end = 7;
+my $on_peak_temp = 20;
+my $mid_peak_temp = 25;
+my $weekend_off_peak_temp = 26;
+my $ulo = 27;
+
+my %hour_map_week_day = (
+	0 => $ulo,
+	1 => $ulo,
+	2 => $ulo,
+	3 => $ulo,
+	4 => $ulo,
+	5 => $ulo,
+	6 => $ulo,
+	7 => $mid_peak_temp,
+	8 => $mid_peak_temp,
+	9 => $mid_peak_temp,
+	10 => $mid_peak_temp,
+	11 => $mid_peak_temp,
+	12 => $mid_peak_temp,
+	13 => $mid_peak_temp,
+	14 => $mid_peak_temp,
+	15 => $mid_peak_temp,
+	16 => $on_peak_temp,
+	17 => $on_peak_temp,
+	18 => $on_peak_temp,
+	19 => $on_peak_temp,
+	20 => $on_peak_temp,
+	21 => $mid_peak_temp,
+	22 => $mid_peak_temp,
+	23 => $ulo
+);
+
+my %hour_map_weekends = (
+	0 => $ulo,
+	1 => $ulo,
+	2 => $ulo,
+	3 => $ulo,
+	4 => $ulo,
+	5 => $ulo,
+	6 => $ulo,
+	7 => $weekend_off_peak_temp,
+	8 => $weekend_off_peak_temp,
+	9 => $weekend_off_peak_temp,
+	10 => $weekend_off_peak_temp,
+	11 => $weekend_off_peak_temp,
+	12 => $weekend_off_peak_temp,
+	13 => $weekend_off_peak_temp,
+	14 => $weekend_off_peak_temp,
+	15 => $weekend_off_peak_temp,
+	16 => $weekend_off_peak_temp,
+	17 => $weekend_off_peak_temp,
+	18 => $weekend_off_peak_temp,
+	19 => $weekend_off_peak_temp,
+	20 => $weekend_off_peak_temp,
+	21 => $weekend_off_peak_temp,
+	22 => $weekend_off_peak_temp,
+	23 => $ulo
+);
+
 my $xmrig_path = `readlink -f $dirname/xmrig`; chomp $xmrig_path;
 print "Resolved xmrig path $xmrig_path\n";
-#19:00 - midnight - 11:00 is off peak
-#weekends is off peak by default
-
-my $max_temp = 26.5;
-my $sensor_to_home_bias = 1;
 
 sub if_temp_too_high {
 	my $socket = new IO::Socket::INET (   
@@ -39,36 +91,35 @@ sub if_temp_too_high {
 		print "buffer is empty, maybe it just during midnight\n";
 		return 0;
 	}
-	my $temp = $buffer - $sensor_to_home_bias;
+	my $temp = $buffer;
+	my $max_temp = shift @_;
+	print "Current max_temp is $max_temp\n";
 	if ($temp > $max_temp) {
 		print "Too hot, we are at $temp\n";
 		return 1;
 	}
+	print "Good temp: $temp\n";
 	return 0;
 }
 
 sub should_run {
-	# if too warm the dont run
-	if(if_temp_too_high()) {
-		return 0;
-	}
-
 	my $now = shift @_;
 	my $run = 0;
 	my $day = `date '+%A' -d \@$now`;
 	chomp $day;
+	my $temp = 0;
 	# check if today is weekends
+	my $hour = `date +%-H -d \@$now`; chomp $hour;
 	if ($day eq "Sunday" or $day eq "Saturday") {
-		$run = 1;
+		$temp = $hour_map_weekends{$hour};
 	} else {
-		my $hour = `date +%-H -d \@$now`; chomp $hour;
-		if ($hour >= $off_peak_end and $hour < $off_peak_start) {
-			$run = 0;
-		} else {
-			$run = 1;
-		}
+		$temp = $hour_map_week_day{$hour};
 	}
-	return $run;
+	# if too warm the dont run
+	if(if_temp_too_high($temp)) {
+		return 0;
+	}
+	return 1;
 }
 
 sub get_xmrig_pid {
@@ -99,7 +150,7 @@ if(should_run($now)) {
 		print "Restart the process\n";
 		my $xmrig = fork();
 		if($xmrig == 0) {
-        		`$xmrig_path &`;
+        		`$xmrig_path > $xmrig_path.log &`;
         		exit;
 		}
 		print "Restarted process: $xmrig\n";
